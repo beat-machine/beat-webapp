bf0b2474ca66acf8b7f78af3740df6b050af3d9e
Joe <computersunified@gmail.com>
2019-06-01 00:08:48 -0400

    Implement a working prototype

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <EffectSelector
-      v-for="(effect, i) in effects"
-      v-bind:key="i"
-      v-bind:effects="allEffects()"
-      ref="effect"
-    ></EffectSelector>
+    <div class="upload">
+      <input type="file" accept="audio/mpeg" v-on:change="updateSelectedSong($event)">
+      <input type="number" v-model.number="bpm">
+      <button v-on:click="submitSong()">submit</button>
+    </div>
+    <div class="effects">
+      <EffectSelector
+        v-for="(effect, i) in effects"
+        v-bind:key="i"
+        v-bind:effects="effectDefinitions"
+        ref="effect"
+      ></EffectSelector>
+    </div>
+    <div class="status">
+      <div>{{status}}</div>
+      <a :href="lastResultUri">Result</a>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import EffectSelector from "./components/EffectSelector.vue";
+import { setInterval, clearInterval } from "timers";
+import effectDefinitions from "./assets/effects.json";
+
+const BASE_URI = "http://beat-api.herokuapp.com";
 
 export default {
   name: "app",
   data() {
     return {
-      effects: [{}, {}]
+      song: null,
+      bpm: 100,
+      effects: [{}],
+      effectDefinitions: effectDefinitions,
+      status: "not processing",
+      pollToken: null,
+      lastResultUri: "#"
     };
   },
   components: {
@@ -25,37 +47,61 @@ export default {
   computed: {
     serializedEffects() {
       return this.$refs.effect.map(e => e.serialized);
+    },
+    requestData() {
+      let data = new FormData();
+
+      data.append("song", this.song);
+      data.append(
+        "data",
+        JSON.stringify({
+          bpm: this.bpm,
+          effects: this.serializedEffects
+        })
+      );
+
+      return data;
     }
   },
   methods: {
-    allEffects() {
-      return [
-        {
-          id: "swap",
-          name: "Swap",
-          description: "Swaps two beats throughout the entire song.",
-          params: [
-            {
-              id: "x_period",
-              name: "Every",
-              value: 2,
-              minimum: 1
-            },
-            {
-              id: "y_period",
-              name: "With",
-              value: 4,
-              minimum: 1
+    updateSelectedSong(e) {
+      this.song = e.target.files[0];
+    },
+    async submitSong() {
+      try {
+        let response = await axios.post(
+          BASE_URI + "/api/v0/submit",
+          this.requestData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data"
             }
-          ]
-        },
-        {
-          id: "randomize",
-          name: "Randomize",
-          description: "Totally randomizes all beats. It's pretty terrible.",
-          params: []
-        }
-      ];
+          }
+        );
+        this.startPolling(response.data.status.href);
+      } catch (e) {
+        console.warn("that's a shame", e);
+      }
+    },
+    startPolling(statusUri) {
+      if (this.pollToken != null) {
+        clearInterval(this.pollToken);
+      }
+
+      this.pollToken = setInterval(
+        async function() {
+          let baseUri = BASE_URI + statusUri;
+          let result = await axios.get(baseUri, { maxRedirects: 0 });
+
+          if (result.data.state === "SUCCESS") {
+            this.lastResultUri = BASE_URI + result.data.location;
+            clearInterval(this.pollToken);
+          } else {
+            console.log(result.data.state);
+          }
+        }.bind(this),
+        3000
+      );
     }
   }
 };
