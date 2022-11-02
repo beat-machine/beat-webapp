6cba788ef71ecf821626f320ecec3bd3657f004c
branchpanic <3802908+branchpanic@users.noreply.github.com>
2022-11-01 20:28:19 -0700

    Remove old Patreon view

--- src/Common/Content.elm
+++ src/Common/Content.elm
@@ -1,22 +1,8 @@
 module Common.Content exposing (..)
 
-import Common.Patreon
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
-
-viewPatreonSection : msg -> String -> Html msg
-viewPatreonSection noOpMsg siteName =
-    section []
-        [ h3 [] [ text "Patreon" ]
-        , p [] [ text <| "Continued development of " ++ siteName ++ " and related projects is made possible by supporters on Patreon." ]
-        , div [ class "patrons" ] <| (Common.Patreon.allPatrons |> List.map (\p -> Html.map (\_ -> noOpMsg) (Common.Patreon.viewPatron p)))
-        , p []
-            [ text "Help keep the site working, contribute to further speed improvements, and promote yourself on this page by "
-            , a [ href "https://www.patreon.com/branchpanic" ] [ text "making a pledge!" ]
-            ]
-        ]
 
 
 type alias FooterInfo =
@@ -52,7 +38,8 @@ viewFooter info =
 viewNavbar : Html msg
 viewNavbar =
     nav []
-        [ a [ href "https://beatmachine.branchpanic.me/" ] [ text "The Beat Machine" ]
-        , a [ href "https://gifsync.branchpanic.me/" ] [ text "Gifsync" ]
+        [ a [ href "https://branchpanic.me/" ] [ text "Home" ]
         , a [ href "https://branchpanic.itch.io/vdcrpt" ] [ text "vdcrpt" ]
+        , a [ href "https://beatmachine.branchpanic.me/" ] [ text "The Beat Machine" ]
+        , a [ href "https://gifsync.branchpanic.me/" ] [ text "Gifsync" ]
         ]
